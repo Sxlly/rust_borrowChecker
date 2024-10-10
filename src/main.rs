d950461e49884a7fb82a2c94f29fa9c84551b685
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-10 14:51:29 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+
+
+fn display_value(item: Vec<u32>) {
+
+    for index in item {
+        println!("{}", index);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let item = vec![1, 2, 3];
+
+    display_value(item);
+    display_value(item);
+
 }
